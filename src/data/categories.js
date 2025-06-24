f6c00572188453fe7598540e38e1c875c53d4950
jys555 <jumanovyashnarbek55@gmail.com>
2025-06-24 10:34:12 +0500

    Create categories.js

---
+++ src/data/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  { id: 1, name: { uz: "Quloqchinlar", ru: "Наушники" }, icon: "🎧" },
+  { id: 2, name: { uz: "Kolonkalar", ru: "Колонки" }, icon: "🔊" },
+  { id: 3, name: { uz: "O'yinchoqlar", ru: "Игрушки" }, icon: "🚗" },
+  { id: 4, name: { uz: "Sport", ru: "Спорт" }, icon: "🏋️" }
+];
+
+export default categories;
