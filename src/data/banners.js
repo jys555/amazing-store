4f759143b257ec12168f5ab531c809064899253a
jys555 <jumanovyashnarbek55@gmail.com>
2025-06-25 01:25:11 +0500

    Create banners.js

---
+++ src/data/banners.js
@@ -0,0 +1,17 @@
+export const banners = [
+  {
+    id: 1,
+    image: "/images/banner_main.jpg",
+    type: "main"
+  },
+  {
+    id: 2,
+    image: "/images/banner_reklama1.jpg",
+    type: "reklama"
+  },
+  {
+    id: 3,
+    image: "/images/banner_reklama2.jpg",
+    type: "reklama"
+  }
+];
